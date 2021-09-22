3027ce3dfc275b8769683598155413c9edb17050
Alexander Chi <arturochi2@hotmail.com>
2021-09-22 15:59:26 -0500

    Se agregó proyecto inicial

---
+++ src/components/GifExpertApp.js
@@ -0,0 +1,17 @@
+/**
+ * @author: alexandercd
+ * @date: 22/septiembre/2021
+ * @description: 
+**/
+import React from 'react';
+
+const GifExpertApp = (props) => {
+    return (
+        <>
+            <h2>GifExpertApp</h2>
+            <hr></hr>
+        </>
+    )
+}
+
+export default GifExpertApp;
---
+++ src/index.js
@@ -0,0 +1,15 @@
+/**
+ * @author: alexandercd
+ * @date: 22/septiembre/2021
+ * @description: 
+**/
+import React from 'react';
+import ReactDOM from 'react-dom'; 
+import GifExpertApp from './components/GifExpertApp';
+import './assets/style/index.css';
+
+ReactDOM.render(
+  <GifExpertApp />,
+  document.getElementById('root')
+);
+ 
